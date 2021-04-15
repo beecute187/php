22a77a6cb777638a92d3b9725fb4b7931fd2a43a
beecute187 <66653369+beecute187@users.noreply.github.com>
2021-04-16 01:00:49 +0700

    Add files via upload

---
+++ db_pktc.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 15, 2021 lúc 08:00 PM
+-- Phiên bản máy phục vụ: 10.4.18-MariaDB
+-- Phiên bản PHP: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `db_pktc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `admin_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id_admin`, `username`, `password`, `admin_status`) VALUES
+(1, 'admin', '8755f7968d9bbf3c7ef67580fc3af4d6ceaa45e6', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_cart`
+--
+
+CREATE TABLE `tbl_cart` (
+  `id_cart` int(11) NOT NULL,
+  `id_khachhang` int(11) NOT NULL,
+  `code_cart` varchar(15) NOT NULL,
+  `cart_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_cart_details`
+--
+
+CREATE TABLE `tbl_cart_details` (
+  `id_cart_details` int(11) NOT NULL,
+  `code_cart` varchar(15) NOT NULL,
+  `id_sanpham` int(11) NOT NULL,
+  `soluong` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_dangky`
+--
+
+CREATE TABLE `tbl_dangky` (
+  `id_dangky` int(11) NOT NULL,
+  `tenkhachhang` varchar(250) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `diachi` varchar(250) NOT NULL,
+  `matkhau` varchar(150) NOT NULL,
+  `dienthoai` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_danhmuc`
+--
+
+CREATE TABLE `tbl_danhmuc` (
+  `id_danhmuc` int(11) NOT NULL,
+  `tendanhmuc` varchar(150) NOT NULL,
+  `thutu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_danhmuc`
+--
+
+INSERT INTO `tbl_danhmuc` (`id_danhmuc`, `tendanhmuc`, `thutu`) VALUES
+(3, 'Thức ăn cho mèo', 2),
+(5, 'Thức ăn cho chó', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_sanpham`
+--
+
+CREATE TABLE `tbl_sanpham` (
+  `id_sanpham` int(11) NOT NULL,
+  `tensp` varchar(250) NOT NULL,
+  `masp` varchar(150) NOT NULL,
+  `giasp` varchar(50) NOT NULL,
+  `soluong` int(11) NOT NULL,
+  `hinhanh` varchar(50) NOT NULL,
+  `tomtat` tinytext NOT NULL,
+  `noidung` text NOT NULL,
+  `tinhtrang` int(11) NOT NULL,
+  `id_danhmuc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_sanpham`
+--
+
+INSERT INTO `tbl_sanpham` (`id_sanpham`, `tensp`, `masp`, `giasp`, `soluong`, `hinhanh`, `tomtat`, `noidung`, `tinhtrang`, `id_danhmuc`) VALUES
+(11, 'Nutri Source', '001', '75000', 1, '1618506292_thuc-an-cho-1.jpg', 'Thức ăn cho chó', 'Thức ăn cho chó hihi', 1, 5),
+(12, 'Royal Canin', '002', '105000', 1, '1618506333_thuc-an-cho-2.jpg', 'Thức ăn cho chó	', 'Thức ăn cho chó haha', 1, 5),
+(13, 'Origi 7', '003', '64000', 1, '1618506453_thuc-an-cho-3.jpg', '...', 'alo ola', 1, 5),
+(14, 'Snappy Tom', '004', '89000', 1, '1618506618_thuc-an-meo-1.jpg', 'meowww', 'meowwwwwww', 1, 3),
+(15, 'Minino Yum', '005', '176000', 1, '1618506657_thuc-an-meo-2.jpg', 'meowwww', 'gruuuuuw', 1, 3),
+(17, 'MeO Presian', '006', '147000', 1, '1618506748_thuc-an-meo-3.jpg', 'graoo', 'bruhh', 1, 3);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Chỉ mục cho bảng `tbl_cart`
+--
+ALTER TABLE `tbl_cart`
+  ADD PRIMARY KEY (`id_cart`);
+
+--
+-- Chỉ mục cho bảng `tbl_cart_details`
+--
+ALTER TABLE `tbl_cart_details`
+  ADD PRIMARY KEY (`id_cart_details`);
+
+--
+-- Chỉ mục cho bảng `tbl_dangky`
+--
+ALTER TABLE `tbl_dangky`
+  ADD PRIMARY KEY (`id_dangky`);
+
+--
+-- Chỉ mục cho bảng `tbl_danhmuc`
+--
+ALTER TABLE `tbl_danhmuc`
+  ADD PRIMARY KEY (`id_danhmuc`);
+
+--
+-- Chỉ mục cho bảng `tbl_sanpham`
+--
+ALTER TABLE `tbl_sanpham`
+  ADD PRIMARY KEY (`id_sanpham`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_cart`
+--
+ALTER TABLE `tbl_cart`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_cart_details`
+--
+ALTER TABLE `tbl_cart_details`
+  MODIFY `id_cart_details` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_dangky`
+--
+ALTER TABLE `tbl_dangky`
+  MODIFY `id_dangky` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_danhmuc`
+--
+ALTER TABLE `tbl_danhmuc`
+  MODIFY `id_danhmuc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_sanpham`
+--
+ALTER TABLE `tbl_sanpham`
+  MODIFY `id_sanpham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
